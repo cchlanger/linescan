4cd83e0e433cb639375916332a2f7724f0b361cb
Christoph Langer <cchlanger@gmail.com>
2024-11-07 16:54:51 +0100

    Implement align_tools for 3 channels

--- align_tools.py
+++ align_tools.py
@@ -26,63 +26,45 @@ def linescan(
             image_path,
             roi_path,
             channels,
-            # This is hacked so bad
+            number_of_channels=number_of_channels,
             align_channel=align_channel,
+            measure_channel= measure_channel,
             line_width=line_width,
             normalize=normalize,
             scaling=scaling,
             align=align,
         )
-        # print(result_df)
         return result_df
     elif number_of_channels == 3:
-        result_df = linescan_3c(
+        # Currently runs the two channel implemention for a pair of measure and align channel
+        result_df = linescan_2c(
             image_path,
             roi_path,
             channels,
+            number_of_channels=number_of_channels,
             align_channel=align_channel,
             measure_channel=measure_channel,
+            line_width=line_width,
             normalize=normalize,
             scaling=scaling,
             align=align,
         )
-        # print(result_df)
         return result_df
-
-
-def linescan_3c(
-    image_path,
-    roi_path,
-    channels,
-    align_channel,
-    measure_channel,
-    scaling,
-    normalize=True,
-    align=True,
-):
-    return linescan_2c(
-        image_path,
-        roi_path,
-        channels,
-        align_channel,
-        normalize=True,
-        scaling=scaling,
-        align=True,
-    )
 
 
 def linescan_2c(
     image_path,
     roi_path,
     channels,
+    number_of_channels,
     align_channel,
-    scaling,
+    measure_channel,
     line_width,
-    normalize=True,
-    align=True,
+    normalize,
+    scaling,
+    align,
 ):
     # get roi and image
-    number_of_channels = 2
 
     def find_nearest(array, value):
         array = np.asarray(array)
@@ -105,9 +87,10 @@ def linescan_2c(
 
         # generate a color map iterator
         cmap = plt.get_cmap("tab10")
-
         for channel in range(number_of_channels):
-            color = cmap.colors[channel]
+            #color = cmap.colors[channel]
+            if channel == measure_channel: color = cmap.colors[0]
+            if channel == align_channel: color = cmap.colors[1]
             # print(newcolor)
             channel_max = []
             # scaling=1
@@ -117,22 +100,23 @@ def linescan_2c(
                 dst = (item["y2"], item["x2"])
 
                 #Calcualte offset for half_maximum
-                values_align_channel = measure_line_values(
-                    image, align_channel, img_slice - 1, src, dst, line_width, number_of_channels
-                )
-                polinomial = np.poly1d(np.polyfit(np.arange(0, len(values_align_channel)), values_align_channel, 10))
-                number_of_interpolation_points = 10*len(values_align_channel)
-                t = np.linspace(0, max(np.arange(0, len(values_align_channel))), number_of_interpolation_points)
-                values_polinomial = (polinomial(t) - min(polinomial(t))) / (max(polinomial(t)) - min(polinomial(t)))
-                closest = find_first_half(values_polinomial)
-                offset = t[closest]
+                if channel == measure_channel or channel == align_channel:
+                    values_align_channel = measure_line_values(
+                        image, align_channel, img_slice - 1, src, dst, line_width, number_of_channels
+                    )
+                    polinomial = np.poly1d(np.polyfit(np.arange(0, len(values_align_channel)), values_align_channel, 10))
+                    number_of_interpolation_points = 10*len(values_align_channel)
+                    t = np.linspace(0, max(np.arange(0, len(values_align_channel))), number_of_interpolation_points)
+                    values_polinomial = (polinomial(t) - min(polinomial(t))) / (max(polinomial(t)) - min(polinomial(t)))
+                    closest = find_first_half(values_polinomial)
+                    offset = t[closest]
 
                 #Saves the align_channel - offset = 0
                 if channel == align_channel:
                     channel_max.append((t[closest] - offset) * scaling)
                 
                 #Caluclates the peak in another channel, saves it minus offset
-                if channel != align_channel:
+                if channel == measure_channel:
                     value_peak_channel = measure_line_values(
                         image, channel, img_slice -1, src, dst, line_width, number_of_channels
                     )
@@ -153,25 +137,27 @@ def linescan_2c(
                     channel_max.append((peak_point - offset) * scaling)
 
                 #Plots all the values - normalized and aligned
-                value_channel = measure_line_values(
-                    image, channel, img_slice - 1, src, dst, line_width, number_of_channels
-                )
-                if normalize == True:
-                    if align == True:
-                        axs.plot(
-                            (np.arange(0, len(value_channel)) - offset) * scaling,
-                            (value_channel - min(value_channel)) / (max(value_channel) - min(value_channel)),
-                            color=color,
-                        )
+                if channel == measure_channel or channel == align_channel:
+                    value_channel = measure_line_values(
+                        image, channel, img_slice - 1, src, dst, line_width, number_of_channels
+                    )
+                    if normalize == True:
+                        if align == True:
+                            axs.plot(
+                                (np.arange(0, len(value_channel)) - offset) * scaling,
+                                (value_channel - min(value_channel)) / (max(value_channel) - min(value_channel)),
+                                color=color,
+                            )
+                        else:
+                            axs.plot(
+                                (np.arange(0, len(value_channel))),
+                                (value_channel - min(value_channel)) / (max(value_channel) - min(value_channel)),
+                                color=color,
+                            )
                     else:
-                        axs.plot(
-                            (np.arange(0, len(value_channel))),
-                            (value_channel - min(value_channel)) / (max(value_channel) - min(value_channel)),
-                            color=color,
-                        )
-                else:
-                    axs.plot(np.arange(0, len(value_channel)), value_channel, color=color)
-            image_peaks[channel].extend(channel_max)
+                        axs.plot(np.arange(0, len(value_channel)), value_channel, color=color)
+            if channel == measure_channel: image_peaks[0].extend(channel_max)
+            if channel == align_channel: image_peaks[1].extend(channel_max)
 
     df = pd.DataFrame(image_peaks)
     df = df.transpose()
